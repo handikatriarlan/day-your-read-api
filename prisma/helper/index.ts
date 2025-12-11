729bfd0e943bd55ce9b592e11e6c554e5eba866a
handikatriarlan <handikaarlan@gmail.com>
2025-12-11 11:24:25 +0700

    chore(prisma): downgrade prisma to 6.18.0
    Update Prisma client import path and initialization logic.
    Explicitly configure the database URL in `schema.prisma`.

--- prisma/helper/index.ts
+++ prisma/helper/index.ts
@@ -1,15 +1,5 @@
-import { PrismaClient } from "@prisma/client"
+import { PrismaClient } from '../generated/prisma/client'
 
-const prismaClientSingleton = () => {
-  return new PrismaClient()
-}
-
-declare global {
-  var prismaGlobal: undefined | ReturnType<typeof prismaClientSingleton>
-}
-
-const prisma = globalThis.prismaGlobal ?? prismaClientSingleton()
+const prisma = new PrismaClient()
 
 export default prisma
-
-if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = prisma
